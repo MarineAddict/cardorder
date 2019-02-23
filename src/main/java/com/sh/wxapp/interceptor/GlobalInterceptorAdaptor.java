63868f0d76a8025af3dc42798cb951e852e37983
xuqie <rick.xu@oyohotels.cn>
2019-02-23 16:51:27 +0800

    exclude swagger request

--- src/main/java/com/sh/wxapp/interceptor/GlobalInterceptorAdaptor.java
+++ src/main/java/com/sh/wxapp/interceptor/GlobalInterceptorAdaptor.java
@@ -3,7 +3,11 @@ package com.sh.wxapp.interceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * GlobalInterceptorAdaptor
@@ -17,7 +21,17 @@ public class GlobalInterceptorAdaptor implements WebMvcConfigurer {
     @Autowired
     private AuthInterceptor authInterceptor;
 
+    private final static String[] EXCLUDE_PATH = {"/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/api-docs", "/login", "/error"};
+    private final static List<String> excludePath = Arrays.asList(EXCLUDE_PATH);
+
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+        registry.addInterceptor(authInterceptor).addPathPatterns("/**").excludePathPatterns(excludePath);
+    }
+
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
